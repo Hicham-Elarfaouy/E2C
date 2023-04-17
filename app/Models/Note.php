20daa2ee56a17770a196777de036aec3967c71dc
Hicham El Arfaouy <hicham.elarfaoui.1@gmail.com>
2023-04-17 02:47:36 +0000

    entity note : define a method for get academic year depend in field created at

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -23,4 +23,12 @@ class Note extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function getAcademicYear(): string
+    {
+        $value = $this->attributes['created_at'];
+        $year = (int)date("Y", strtotime($value));
+        $month = (int)date("m", strtotime($value));
+        $year = ($month >= 9) ? $year : $year - 1;
+        return $year . ' / ' . $year + 1;
+    }
 }
