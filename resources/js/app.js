83c1d90017b593b9363e117a01035ba03c2a0554
Hicham El Arfaouy <hicham.elarfaoui.1@gmail.com>
2023-04-18 12:40:19 +0000

    implement chart js

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,10 @@ Alpine.plugin(focus);
 
 Alpine.start();
 
+// chart js
+// import Chart from 'chart.js/auto';
+// window.Chart = Chart;
+
 // DataTable
 import DataTable from 'datatables.net-dt';
 // import 'datatables.net-responsive-dt';
